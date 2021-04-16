5c4ca4261f9c7d1a448aaf40ec129e3b8eba2cd8
Mykhailo1995 <m.stefaniuk.1995@gmail.com>
2021-04-16 15:41:51 +0300

    test

--- src/test/java/test/LocalTestRunner.java
+++ src/test/java/test/LocalTestRunner.java
@@ -1,4 +1,28 @@
 package test;
+package seleniumpackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class LocalTestRunner {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        String baseWebUrl = "https://www.linkedin.com/";
+        String expectedWebsiteTitle = "World’s Largest Professional Network | LinkedIn";
+        String actualWebsiteTitle = "";
+
+        driver.get(baseWebUrl);
+
+        actualWebsiteTitle = driver.getTitle();
+
+        if (actualWebsiteTitle.contentEquals(expectedWebsiteTitle)) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+        }
+
+        driver.close();
+
+        System.exit(0);
+
+    }
 }
